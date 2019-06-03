8a04013e763f4756e4d396f7a6710513bbbf6c5b
carl12 <carl17r@gmail.com>
2019-06-03 11:32:34 -0700

    Add terse set solution

--- ContainsDuplicate/ContainsDuplicate.js
+++ ContainsDuplicate/ContainsDuplicate.js
@@ -6,3 +6,9 @@ const containsDuplicate = (nums) => {
   let chars = new Set();
   return nums.reduce((duplicate, val) => duplicate || chars.has(val) || !chars.add(val), false);
 };
+
+const containsDuplicate2 = (nums) => {
+  return new Set(nums).size !== nums.length;
+};
+
+
