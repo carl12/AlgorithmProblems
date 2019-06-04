d61f6a98cf04891701483967ee582da97cbdc937
carl12 <carl17r@gmail.com>
2019-06-04 15:07:00 -0700

    Add one array solution

--- ArrayProduct/ArrayProduct.js
+++ ArrayProduct/ArrayProduct.js
@@ -22,18 +22,17 @@ var productExceptSelf = function(nums) {
  * @param {number[]} nums
  * @return {number[]}
  */
-var productExceptSelf2 = function(nums) {
+var productExceptSelf = function(nums) {
   let store = new Array(nums.length);
   store[0] = nums[0];
   for (var i = 1; i < nums.length; i++) {
-      
       store[i] = store[i - 1] * nums[i];
   }
+  let right = 1;
   for (var i = nums.length - 1; i >= 0; i --) {
-      let nP1 = nums[i + 1] === undefined ? 1 : nums[i + 1];
       let sM1 = store[i - 1] === undefined? 1 : store[i - 1];
-      store[i] = sM1 * nP1;
-      nums[i] = nP1 * nums[i];
+      store[i] = sM1 * right;
+      right *= nums[i];
   }
   return store;
 };
