da4c7409701b3efcc2cb63626e8de89c1f884fd2
carl12 <carl17r@gmail.com>
2019-06-04 14:47:59 -0700

    Add solution which only allocates one arr

--- ArrayProduct/ArrayProduct.js
+++ ArrayProduct/ArrayProduct.js
@@ -17,3 +17,23 @@ var productExceptSelf = function(nums) {
   }
   return store;
 };
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf2 = function(nums) {
+  let store = new Array(nums.length);
+  store[0] = nums[0];
+  for (var i = 1; i < nums.length; i++) {
+      
+      store[i] = store[i - 1] * nums[i];
+  }
+  for (var i = nums.length - 1; i >= 0; i --) {
+      let nP1 = nums[i + 1] === undefined ? 1 : nums[i + 1];
+      let sM1 = store[i - 1] === undefined? 1 : store[i - 1];
+      store[i] = sM1 * nP1;
+      nums[i] = nP1 * nums[i];
+  }
+  return store;
+};
