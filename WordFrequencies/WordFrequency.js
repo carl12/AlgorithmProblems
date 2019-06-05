62b887b7d26795d3dc9b0249feb6a269a0cb6a7c
carl12 <carl17r@gmail.com>
2019-06-05 10:59:59 -0700

    Add word frequency problem

---
+++ WordFrequencies/WordFrequency.js
@@ -0,0 +1,70 @@
+function WordFrequencies(words, text) {
+    const count = {};
+    const myTrie = new Trie(words);
+    for (let word of words) {
+        count[word] = 0;
+    }
+    let curr;
+    for (let char of text) {
+        console.log(char);
+        if (char === ' ') {
+            if (curr && curr.finishedWord) {
+                count[curr.finishedWord] += 1;
+                console.log('found: ', curr.finishedWord);
+            }
+            curr = myTrie.root;
+            continue;
+        }
+        if (!curr) {
+            continue;
+        }
+        curr = curr.children[char] || null;
+    }
+    if (curr && curr.finishedWord) {
+        count[curr.finishedWord] += 1;
+        console.log('found: ', curr.finishedWord);
+    }
+    const res = [];
+    for (let word of words) {
+        res.push(count[word]);
+    }
+    return res;
+}
+class Trie {
+    constructor(words) {
+        this.root = this.makeNode();
+        for (let word of words) {
+            let curr = this.root;
+            for (let char of word) {
+                if (!curr.children[char]) {
+                    curr.children[char] = this.makeNode();
+                }
+                curr = curr.children[char];
+            }
+            curr.finishedWord = word;
+        }
+    }
+    makeNode(finishedWord = null) {
+        return { finishedWord: null, children: {} };
+    }
+}
+function WordFrequencies2(words, text) {
+    let textWords = text.split(' ');
+    let count = {};
+    for (let word of words) {
+        count[word] = 0;
+    }
+    for (let textWord of textWords) {
+        if (count[textWord] !== undefined) {
+            count[textWord]++;
+        }
+    }
+    const res = [];
+    for (let word of words) {
+        res.push(count[word]);
+    }
+    console.log(count);
+    return res;
+}
+WordFrequencies2(['asdf', 'a', 'b', 'sdf'], 'asdf a b asdf asd sdf c d e asdf asdf a');
+//# sourceMappingURL=WordFrequency.js.map
---
+++ WordFrequencies/WordFrequency.ts
@@ -0,0 +1,95 @@
+/*
+Design a method to find the frequency of occurrences of any given word 
+in a book. What if we were running this algorithm multiple times?
+
+Key Takeaway: don't implement a whole datastructure to cut down on a 
+pass from splitting
+*/
+function WordFrequencies(words: string[], text: string): Number[] {
+  const count = {};
+  const myTrie = new Trie(words);
+  for (let word of words) {
+    count[word] = 0;
+  }
+  let curr: (TrieNode | null);
+  for(let char of text) {
+    console.log(char)
+    if (char === ' ') {
+      if (curr && curr.finishedWord) {
+        count[curr.finishedWord] += 1;
+        console.log('found: ', curr.finishedWord);
+      }
+      curr = myTrie.root;
+      continue;
+    } 
+    if (!curr) {
+      continue;
+    }
+    curr = curr.children[char] || null;
+  }
+
+  if (curr && curr.finishedWord) {
+    count[curr.finishedWord] += 1;
+    console.log('found: ', curr.finishedWord);
+  }
+
+  const res: Number[] = [];
+  for (let word of words) {
+    res.push(count[word]);
+  }
+  return res;
+}
+
+interface TrieNode {
+  finishedWord: (string | null),
+  children: {[key: string]: TrieNode}
+}
+
+class Trie {
+  root: TrieNode;
+  constructor(words: string[]) {
+    this.root = this.makeNode();
+    for (let word of words) {
+      let curr = this.root;
+      for (let char of word) {
+        if (!curr.children[char]) {
+          curr.children[char] = this.makeNode();
+        }
+        curr = curr.children[char];
+      }
+      curr.finishedWord = word;
+    }
+  }
+
+
+
+  makeNode(finishedWord = null): TrieNode {
+
+    return {finishedWord: null, children: {}};
+  }
+}
+
+function WordFrequencies2(words: string[], text: string): Number[] {
+  let textWords = text.split(' ');
+  let count = {};
+  for (let word of words) {
+    count[word] = 0;
+  }
+
+  for (let textWord of textWords) {
+    if (count[textWord] !== undefined) {
+      count[textWord] ++;
+    }
+  } 
+
+  const res: Number[] = [];
+  for (let word of words) {
+    res.push(count[word]);
+  }
+  console.log(count)
+  return res;
+}
+// let a = new Trie(['asdf']);
+// console.log(a.root.children.a.children.s.children.d.children.f);
+
+WordFrequencies2(['asdf', 'a', 'b', 'sdf'], 'asdf a b asdf asd sdf c d e asdf asdf a');
