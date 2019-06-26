abf0204040cb3bccc0b6d59137c615e66f79528a
carl12 <carl17r@gmail.com>
2019-06-26 16:54:50 -0700

    Add subsort optimized solution and some tests

--- SubSort/Subsort.js
+++ SubSort/Subsort.js
@@ -1,4 +1,5 @@
- 
+ let rnd = require('../GenericRandoms');
+
 function subSort(arr) {
   let lastInPlace = arr.length - 1;
   let firstInPlaceFromEnd = 0;
@@ -25,9 +26,6 @@ function subSort(arr) {
   let minMiddle = Math.min(...middle);
   let maxMiddle = Math.max(...middle);
 
-  console.log(middle)
-  console.log(minMiddle, maxMiddle);
-
   let left = lastInPlace;
   while (left > 0 && minMiddle <= arr[left - 1]) {
     left --;
@@ -35,12 +33,49 @@ function subSort(arr) {
   
   let right = firstInPlaceFromEnd;
   while (right < arr.length - 1 && maxMiddle > arr[right + 1]) {
-    console.log(right)
     right ++;
   }
-  console.log(left, right);
 
   return [left, right];
 }
 
-subSort([-10,2,3,4,-1, 2, 20, 2, 3, 4, 50]);
+function subSort2(arr) {
+  let left = findFurthestLeftGreaterThanMin(arr);
+  let right = findFurthestRightLessThanMax(arr);
+  return [left, right];
+}
+
+function findFurthestLeftGreaterThanMin(arr) {
+  let min = arr[arr.length - 1];
+  let minLoc = 0;
+  for (let i = arr.length - 2; i >= 0; i --) {
+    if (arr[i] > min) {
+      minLoc = i;
+    } else {
+      min = arr[i];
+    }
+  }
+  return minLoc;
+}
+
+function findFurthestRightLessThanMax(arr) {
+  let max = arr[0];
+  let maxLoc = 0;
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < max) {
+      maxLoc = i;
+    } else {
+      max = arr[i];
+    }
+  }
+  return maxLoc;
+}
+
+// for (var i = 0; i < 10000; i++ ){ 
+//   let arr = rnd.randomFloatArr(4);
+//   let a = subSort(arr);
+//   let b = subSort2(arr);
+//   if (a[0] !== b[0] || a[1] !== b[1]) {
+//     console.log(a, b);
+//   }
+// }
