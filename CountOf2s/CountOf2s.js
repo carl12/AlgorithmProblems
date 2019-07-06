6ea4b802a4fa7135b150328ac705d784edc586a8
carl12 <carl17r@gmail.com>
2019-07-05 17:27:11 -0700

    Add entered solution and fix errors

--- CountOf2s/CountOf2s.js
+++ CountOf2s/CountOf2s.js
@@ -8,9 +8,8 @@ function countOf2s(n){
 
   let level = 1;
   let count = 0;
-  while (level < n) {
+  while (level <= n) {
     let digit = Math.floor(n % (level * 10) / level);
-
     count += amounts(level) * digit;
     if (digit === 2) {
       count += n % level + 1;
@@ -19,7 +18,7 @@ function countOf2s(n){
     }
     level *= 10;
   }
-  console.log(count);
+
   return count;
 
   function amounts(level) {
@@ -27,4 +26,39 @@ function countOf2s(n){
     return (10 ** (tier - 1)) * tier;
   }
 }
+
+function countOf2s2(n) {
+  let count = 0;
+  len = n.toString().length;
+  for (let digit = 0; digit < len; digit ++) {
+    count += count2sInRange(n, digit);
+  }
+  return count;
+
+  function count2sInRange(n, d) {
+    let powerOf10 = 10 ** d;
+    let nextPowerof10 = powerOf10 * 10;
+    let right = n % powerOf10;
+
+    let roundDown = n - n % nextPowerof10;
+    let roundUp = roundDown + nextPowerof10;
+
+    let digit = Math.floor(n / powerOf10) % 10;
+    if (digit < 2) {
+      return Math.floor(roundDown / 10);
+    } else if (digit === 2) {
+      return Math.floor(roundDown / 10) + right + 1;
+    } else {
+      return Math.floor(roundUp / 10);
+    }
+  }
+}
+
+
+// for (let i = 0; i < 100000; i++) {
+//   if (countOf2s(i) !== countOf2s2(i)) {
+//     console.log(`${i}: ${countOf2s(i)}, ${countOf2s2(i)}`);
+//     return;
+//   }
+// }
 
