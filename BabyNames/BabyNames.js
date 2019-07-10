fbd2dfbb52983bcdf5028bdff5555ab1fdd5bef5
carl12 <carl17r@gmail.com>
2019-07-09 17:19:27 -0700

    Add baby names solution that properly handles merging groups of names

--- BabyNames/BabyNames.js
+++ BabyNames/BabyNames.js
@@ -5,7 +5,7 @@ function babyNames(names, equivalents){
 
   setUpHashMap(equivalents, nameToCount);
   addNamesToMap(names, nameToCount);
-
+  console.log(output);
   return output;
 
   function setUpHashMap(equivalents, nameToCount) {
@@ -39,20 +39,63 @@ function m(name, frequency) {
   return {name, frequency};
 }
 
+function babyNamesCorrect(names, equivalents) {
+  let nameToNode = {};
+  let nameFreqs = [];
+  
+  for (let entry of names) {
+    nameToNode[entry.name] = new Node(entry);
+  }
 
-// babyNames([
-//   m('joe', 4), 
-//   m('bob', 10), 
-//   m('cindy', 20), 
-//   m('frank', 10), 
-//   m('steve', 10),
-//   m('al', 11),
-//   m('fred', 12),
+  for (let pair of equivalents) {
+    nameToNode[pair[0]].addConnectionTo(nameToNode[pair[1]]);
+  }
 
-// ], [
-//   ['joe', 'bob'], 
-//   ['frank', 'bob'],
-//   // ['steve', 'joe'],
-//   ['bob', 'al'],
-//   ['steve', 'fred'],
-// ]);
+  for (let entry of names) {
+    let node = nameToNode[entry.name];
+    if (node.visited) {
+      continue;
+    }
+    nameFreqs.push({name: node.name, frequency: getSum(node)});
+  }
+  console.log(nameFreqs);
+  return nameFreqs;
+
+  function Node({ name, frequency }) {
+    this.name = name;
+    this.frequency = frequency;
+    this.visited = false;
+    this.neighbors = [];
+
+    this.addConnectionTo = function(otherNode) {
+      this.neighbors.push(otherNode);
+      otherNode.neighbors.push(this);
+    }
+  }
+
+  function getSum(node) {
+    if (node.visited) {
+      return 0;
+    }
+    node.visited = true;
+    let sum = node.frequency;
+    node.neighbors.forEach(neighbor => sum += getSum(neighbor));
+    return sum;
+  }
+}
+
+
+babyNamesCorrect([
+  m('joe', 4), 
+  m('bob', 10), 
+  m('cindy', 20), 
+  m('frank', 10), 
+  m('steve', 10),
+  m('al', 11),
+  m('fred', 12),
+
+], [
+  ['joe', 'bob'],
+  ['cindy', 'frank'],
+  ['joe', 'cindy'],
+]);
