0b4a22d47c78312ac81034dc31b9f9985a4ab175
carl12 <carl17r@gmail.com>
2019-08-11 14:33:51 -0700

    Update paths with sum with takeaway

--- PathsWithSum/PathsWithSum.js
+++ PathsWithSum/PathsWithSum.js
@@ -39,3 +39,35 @@ b.left = d;
 
 
 console.log(pathsWithSum(a, 10));
+
+// initial solution elegant but inefficient
+// break down problem -> paths to leaf are basically a linked list -> find contiguous subsequence with sum 
+  // thus, enter all running sums into hash table (keep count of each value to handle duplicates)
+  // Do lookup in hash for current sum - target
+  // if different exists in hash there is a subsequence
+  // 1, 2, 3, 4, 5, -2 + looking for 7
+  // {1,} 
+  // {1, 3} + lookup -4 X
+  // {1, 3, 6} + lookup -1 X
+  // {1, 3, 6, 10} + lookup 3 ++
+  // {1, 3, 6, 10, 15} + lookup 8 X
+  // {1, 3, 6, 10, 15, 13} + lookup 6 ++
+
+  // return 2
+// With this approach you can incrementally add and remove to hash as you traverse
+// Maximum hash size is depth of tree
+
+
+// old approach is O(N * depth), this one is O(N)
+
+// ---------------
+// Takeaways 
+// ---------------
+
+// Don't stop at elegant recursive solution
+// Break down essence of problem (finding contiguous subsequence in arr)
+
+// 
+
+
+
